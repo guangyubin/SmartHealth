f414307e5abedee90327af2fcb622567f8df067a
LiRenxin <43311755+LiRenxin@users.noreply.github.com>
2018-09-21 18:40:32 +0800

    Update ecg.m

--- 2018/students/S201815706/ecg.m
+++ 2018/students/S201815706/ecg.m
@@ -7,8 +7,8 @@
  fmaxn = fmaxd/(fs/2);
  [b,a] = butter(1,fmaxn,'low');
  [e,f] = butter(1,20/(250/2),'low');
- f1 = filter(b,a,d);
- f2 = d-f1;
+ f1 = filter(b,a,d);%通过5Hz低通滤波器的信号
+ f2 = d-f1;%获得去基线漂移的信号
  subplot(211),plot(d(1000:4000),'b');
  title('初始信号');
  subplot(212),plot(f2(1000:4000),'b');
