95d132271a457e6ae459bc013ddd49f0b9a367aa
xxjimi <43269389+xxjimi@users.noreply.github.com>
2018-09-21 21:24:52 +0800

    Add files via upload

--- 2018/students/S201815052/ECG_filter.m
+++ 2018/students/S201815052/ECG_filter.m
@@ -6,8 +6,9 @@ fs=250; %采样率
 Wn=2*f/fs;
 [b,a]=butter(2,Wn,'low');   %2阶巴特沃斯低通系数
 y=filtfilt(b,a,ECG_data);   %0相移滤波
-ECG_filter=ECG_data-y;          
+ECG_filter1=ECG_data-y;          
 %绘图
 subplot(2,1,1),plot(ECG_data(1000:5000),'b');
-subplot(2,1,2),plot(ECG_filter(1000:5000),'b');
+subplot(2,1,2),plot(ECG_filter1(1000:5000),'b');
+
 
