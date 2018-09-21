8a4f6c4481b5d6440b8fc422262eb600add0e85a
LiRenxin <43311755+LiRenxin@users.noreply.github.com>
2018-09-21 19:13:56 +0800

    Update ecg.m

--- 2018/students/S201815706/ecg.m
+++ 2018/students/S201815706/ecg.m
@@ -1,7 +1,7 @@
  fid = fopen('1520309088000.dat','r');
  d = fread(fid,Inf,'short');
  fclose(fid);
- fmaxd = 5;%截止频率为5Hz
+ fmaxd = 0.5;%截止频率为0.5Hz
  fs = 250;%采样率250
  fmaxn = fmaxd/(fs/2);
  [b,a] = butter(1,fmaxn,'low');
