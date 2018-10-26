ecd94022625c8655aadf375e99b8aee66df9e7bf
LiRenxin <43311755+LiRenxin@users.noreply.github.com>
2018-10-26 11:26:31 +0800

    Add files via upload

---
+++ 2018/students/S201815706/m file/r.m
@@ -0,0 +1,20 @@
+ clc;
+ clear;
+ fid = fopen('../../100.dat','rb');
+ sig = fread(fid,inf,'short');  
+ fclose(fid);  
+ L=length(sig);  
+ fs=250;  
+ t=1/fs:1/fs:(L-1)/fs;
+ time = (0:L-1)/fs;
+ [b,a]= butter(2,[8 20]/(fs/2));
+ sig1=filter(b,a,sig);
+ sig1=diff(sig1); 
+ sig2=abs(sig1); 
+ sig3=filter(ones(1,5)/5,1,sig2);
+%---------------painting---------------%
+ figure(3);
+ subplot(411);plot(time,sig);xlim([200 210]);title('原始信号');xlabel('T(s)');ylabel('A(mv)');
+ subplot(412);plot(t,sig1);xlim([200 210]);title('滤波后信号');xlabel('T(s)');ylabel('A(mv)');
+ subplot(413);plot(t,sig2);xlim([200 210]);title('对滤波后信号取绝对值');xlabel('T(s)');ylabel('A(mv)');
+ subplot(414);plot(t,sig3);xlim([200 210]);title('对滤波后信号的绝对值滤波');xlabel('T(s)');ylabel('A(mv)');
