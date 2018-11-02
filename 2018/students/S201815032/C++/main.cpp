20ddc5c676a338cceaecac1a281d73bdb4198105
1SongXiaoxiaoS201815032 <43269394+1SongXiaoxiaoS201815032@users.noreply.github.com>
2018-11-02 22:22:01 +0800

    Add files via upload

---
+++ 2018/students/S201815032/C++/main.cpp
@@ -0,0 +1,58 @@
+//源文件里是一些函数定义（也有说是函数实现，总之就是函数的具体内容），和主函数。
+#include<stdio.h>
+#include"QrsDetect.h"
+
+
+void main()
+{
+	printf("hello world!\n");
+	FILE *fp =  fopen("C:\\Users\\HP\\Desktop\\qrsdetecg\\data\\100.dat", "rb");
+	FILE *fp1 = fopen("C:\\Users\\HP\\Desktop\\qrsdetecg\\data\\100_filt.dat", "wb");
+	FILE *fp2 = fopen("C:\\Users\\HP\\Desktop\\qrsdetecg\\data\\100.txt", "w+");
+	short x;
+	int pos = 0;
+	int i; 
+
+ int total = 0;
+
+	while (fread(&x, sizeof(short), 1, fp))
+	{
+
+
+		short y = smooth(diff(bpfilter(x)));
+		fwrite(&y, sizeof(short), 1, fp1);
+		//	printf("%d",x);
+		int res = QrsDectet(x);
+		if (res == 1)   
+		{
+			
+		     total = total ++;
+			fprintf(fp2,"%d \n" ,i );
+			printf("there is a qrs in samples  %d \n", i);
+		}
+	}
+	printf("the total beats is   %d \n", total);
+	
+	fclose(fp);
+	fclose(fp1);
+	fclose(fp2);
+
+}
+
+// matlab code
+
+/*              
+//fid = fopen('C:\Users\guang\Desktop\SmartHealth\2018\data\100.dat', 'rb');
+//d = fread(fid, inf, 'short');
+//fclose(fid);
+//
+//fid = fopen('C:\Users\guang\Desktop\SmartHealth\2018\data\100_filt.dat', 'rb');
+//d2 = fread(fid, inf, 'short');
+//fclose(fid);
+//figure;
+//subplot(211); plot(d2);
+//
+//qrs = load('C:\Users\guang\Desktop\SmartHealth\2018\data\100.txt');
+//
+//subplot(212); plot(d2); hold on; plot(qrs, d2(qrs), '.r');
+*/
